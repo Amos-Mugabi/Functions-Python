c533e4e3698c1632ac017f54484d229ca3933473
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-03 10:14:06 +0300

    Add files via upload

---
+++ morefuncs.py
@@ -0,0 +1,16 @@
+#calling functions
+#non-parameterized functions
+asks_name = "Martin"
+
+def asks_name():
+    name = input("What is your name?")
+    print(name)
+
+print("we are starting to call the function here")
+print(asks_name)
+asks_name()
+
+asks_name()
+
+asks_name()
+print("we are ending to call the function here")
