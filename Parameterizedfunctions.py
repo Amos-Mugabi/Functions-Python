17d4059f7dd357ad6e5472dfeadb38795a06beb1
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-06 13:25:56 +0300

    Add files via upload

---
+++ Parameterizedfunctions.py
@@ -0,0 +1,20 @@
+#Parameterized functions
+
+"""
+syntax:
+    def func_name(parameter):
+        body
+
+when calling a function with a parameter,the data value is called an argument
+"""
+def greeting(name):
+            print("Good morning, ", name)
+
+
+
+greeting("amos")
+       
+
+
+            
+            
