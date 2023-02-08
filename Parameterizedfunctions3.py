8f8832290978d300d38b84a49491a0079f92c6e1
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-02-08 13:11:25 +0300

    Add files via upload

---
+++ Parameterizedfunctions3.py
@@ -0,0 +1,20 @@
+#keyword arguments
+
+
+
+
+def introduction(first_name="martin", last_name="kananura"):
+    print(first_name + " " + last_name)
+
+
+
+
+
+#calling without arguments
+introduction()
+
+#replacing default arguments
+introduction("martin", "seth")
+
+#supplying one value
+introduction("martin")
